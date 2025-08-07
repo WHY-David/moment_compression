968fac2909b6a63db633751fbe886668638e4789
WHY-David <hongyi1602@gmail.com>
2025-08-07 12:38:10 -0400

    .

--- gpu/compress_train_ds.py
+++ gpu/compress_train_ds.py
@@ -34,7 +34,7 @@ def compress_train_ds(train_ds, return_list, k=1, tol=1e-12):
 if __name__ == '__main__':
     seed = 0
     d = 100_000
-    k = 1
+    k = 3
     print(f"k = {k}")
 
     train_ds = generate_train_data(d, noise=0.5, seed=seed)
